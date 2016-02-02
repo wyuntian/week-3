f6c0064c40aa1b4624c3a1b684aeb5f5b37d047b
wyuntian <wyuntian@design.upenn.edu>
2016-02-02 14:05:08 -0500

    work through lab

--- lab/lab1/part2.js
+++ lab/lab1/part2.js
@@ -8,8 +8,8 @@
 Instructions: Write a function that *always* returns the number 1.
 ===================== */
 
-var justOne = function() {
-
+var justOne = function(number) {
+    return 1
 };
 
 console.log('justOne success:', justOne() === 1);
@@ -18,7 +18,13 @@ console.log('justOne success:', justOne() === 1);
 Instructions: Write a function that returns true if a number is even.
 ===================== */
 
-var isEven = function() {
+var isEven = function(number) {
+  if(number%2===0){
+  return true
+}
+else{
+  return false
+}
 
 };
 
@@ -29,8 +35,8 @@ Instructions: Write a function that *always* returns false.
               Use functions "justOne" and "isEven" somehow in the definition.
 ===================== */
 
-var justFalse = function() {
-
+var justFalse = function(number) {
+    return isEven(justOne(number))
 };
 
 console.log('justFalse success:', justFalse() === false);
@@ -39,7 +45,13 @@ console.log('justFalse success:', justFalse() === false);
 Instructions: Write a function that takes a boolean value and returns its opposite.
 ===================== */
 
-var not = function() {
+var not = function(value) {
+  if (value===true){
+    return false
+  }
+  else{
+    return true
+  }
 
 };
 
@@ -50,8 +62,8 @@ Instructions: Write a function that returns true if a number is odd
               Use functions "isEven" and "not" somehow in the definition.
 ===================== */
 
-var isOdd = function() {
-
+var isOdd = function(number) {
+    return not(isEven(number))
 };
 
 console.log('isOdd success:', isOdd(4) === false);
@@ -60,11 +72,19 @@ console.log('isOdd success:', isOdd(4) === false);
 Instructions: Write a function that takes a list of numbers and returns a list with only numbers above 10
 ===================== */
 
-var filterOutLessThan10 = function() {
+var filterOutLessThan10 = function(list) {
+            var myarray=[];
+            for(var i= 0; i<list.length; i++){
+              if(list[i]>=10){
+                myarray.push(list[i]);
+
+              }
+            }
+            return(myarray)
 
 };
 
-console.log('filterOutLessThan10 success:', filterOutLessThan10([4, 11]) === [11]);
+console.log('filterOutLessThan10 success:', filterOutLessThan10([4, 11]).length === 1);
 
 /* =====================
 Stretch goal — NOT required
